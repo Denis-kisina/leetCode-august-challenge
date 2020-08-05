52036d9096a4e989c1c79560f91180b589a9615e
Denis Kisina <kisinad@gmail.com>
2020-08-04 20:33:44 -0500

    isPowerOfFour

---
+++ leetcode-august-challenge/src/com/denis/kisina/wee1/IsPowerOfFour.java
@@ -0,0 +1,27 @@
+package com.denis.kisina.wee1;
+
+public class IsPowerOfFour {
+	public boolean isPowerOfFour(int num) {
+		if (num == 0) {
+			return false;
+		}
+//         int temp = num;
+//         int count = 1;
+//         while(temp%4==0 && temp != 4){
+//             temp = temp/4;
+//             count++;
+//         }
+//         if(Math.pow(4, count) == num){return true;}
+
+//         return false;
+//     }
+
+		int pow = (int) (Math.log(num) / Math.log(4));
+
+		if (Math.pow(4, pow) == num) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
