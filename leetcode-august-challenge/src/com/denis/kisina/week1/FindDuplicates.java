88a2f8cd69822329e4d033fae256e4b8ed3f56f1
Denis Kisina <kisinad@gmail.com>
2020-08-10 00:23:41 -0500

    rottenOranges

---
+++ leetcode-august-challenge/src/com/denis/kisina/week1/FindDuplicates.java
@@ -0,0 +1,13 @@
+package com.denis.kisina.week1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class FindDuplicates {
+	public List<Integer> findDuplicates(int[] nums) {
+		List<Integer> list = new ArrayList<>();
+
+		return null;
+
+	}
+}
